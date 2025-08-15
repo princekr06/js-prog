7871eeebcb4e3e2b59eb79d64042eb87660093cc
princekr06 <princeram4000@gmail.com>
2025-08-15 08:04:01 +0000

    done

---
+++ basic/dates.js
@@ -0,0 +1,9 @@
+let myDate= new Date()
+//console.log(myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleDateString());
+//console.log(typeof myDate)
+
+let mycreatedate= new Date(2023,0,23)
+console.log(mycreatedate.toDateString())
